eba078b796b53aa775a06779db8bc29cdfa5f307
David <sky80420@hotmail.com>
2022-04-04 19:39:12 +0800

    Update odds.vue

--- src/views/odds.vue
+++ src/views/odds.vue
@@ -865,7 +865,6 @@ export default {
                 default:
             }
         }
-        // function getOddsLine() {}
         watch(betRateSelectValue, () => {
             betRateConvert()
         })
